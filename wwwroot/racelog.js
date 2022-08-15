c97f515437913ea4c6ea2844f91157682858961e
rsfost <81119640+rsfost@users.noreply.github.com>
2022-08-15 14:51:43 -0400

    Put functions under a namespace

--- wwwroot/racelog.js
+++ wwwroot/racelog.js
@@ -1,70 +1,72 @@
-let db;
-const DB_VERSION = 1;
+(function() {
+    let db;
+    const DB_VERSION = 1;
 
-function mapRow(row) {
-    return {
-        date: row[0],
-        race: row[2],
-        captain: row[3],
-        pick1: row[4],
-        pick2: row[5],
-        pick3: row[6],
-        pick4: row[7],
-        time: row[8],
-        position: row[9]
-    };
-}
-
-function transaction() {
-    const tx = db.transaction(["races"], "readwrite");
-    return tx;
-}
-
-function objectStore(tx) {
-    tx = tx || transaction();
-    const objectStore = tx.objectStore("races");
-    return objectStore;
-}
-
-function fillDb(callback) {
-    const fetchRacelog = fetch('./data/racelog.json').then((resp) => resp.json());
-    fetchRacelog.then(racelog => {
-        const tx = transaction();
-        const decrementLatch = (() => {
-            let latch = racelog.values.length;
-            return () => {
-                if (--latch <= 0) {
-                    callback(db);
-                }
+    racelog.init = function(callback) {
+        const req = window.indexedDB.open("racelog", DB_VERSION);
+        req.onerror = (event) => {
+            console.log(event);
+        };
+        req.onsuccess = (event) => {
+            db = event.target.result;
+            racelog.objectStore().clear().onsuccess = (event) => {
+                fillDb(callback);
             };
-        })();
-        for (let i = 0; i < racelog.values.length; ++i) {
-            const addReq = objectStore(tx).add({
-                id: i,
-                ...mapRow(racelog.values[i])
+        };
+        req.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            db.createObjectStore("races", {
+                keyPath: "id",
+                autoIncrement: false
             });
-            addReq.onsuccess = (event) => decrementLatch();
-            addReq.onerror = (event) => decrementLatch();
-        }
-    });
-}
-
-function init(callback) {
-    const req = window.indexedDB.open("racelog", DB_VERSION);
-    req.onerror = (event) => {
-        console.log(event);
-    };
-    req.onsuccess = (event) => {
-        db = event.target.result;
-        objectStore().clear().onsuccess = (event) => {
-            fillDb(callback);
         };
     };
-    req.onupgradeneeded = (event) => {
-        const db = event.target.result;
-        db.createObjectStore("races", {
-            keyPath: "id",
-            autoIncrement: false
-        });
+
+    racelog.transaction = function() {
+        const tx = db.transaction(["races"], "readwrite");
+        return tx;
     };
-}
+
+    racelog.objectStore = function(tx) {
+        tx = tx || racelog.transaction();
+        const objectStore = tx.objectStore("races");
+        return objectStore;
+    };
+
+    function mapRow(row) {
+        return {
+            date: row[0],
+            race: row[2],
+            captain: row[3],
+            pick1: row[4],
+            pick2: row[5],
+            pick3: row[6],
+            pick4: row[7],
+            time: row[8],
+            position: row[9]
+        };
+    }
+
+    function fillDb(callback) {
+        const fetchRacelog = fetch('./data/racelog.json').then((resp) => resp.json());
+        fetchRacelog.then(data => {
+            const tx = racelog.transaction();
+            const decrementLatch = (() => {
+                let latch = data.values.length;
+                return () => {
+                    if (--latch <= 0) {
+                        callback();
+                    }
+                };
+            })();
+            for (let i = 0; i < data.values.length; ++i) {
+                const addReq = racelog.objectStore(tx).add({
+                    id: i,
+                    ...mapRow(data.values[i])
+                });
+                addReq.onsuccess = (event) => decrementLatch();
+                addReq.onerror = (event) => decrementLatch();
+            }
+        });
+    }
+})(window.racelog = window.racelog || {}, undefined);
