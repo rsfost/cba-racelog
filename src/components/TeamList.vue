2b7192575b574ac8d5338360af261f4969d4cb54
rsfost <81119640+rsfost@users.noreply.github.com>
2022-08-20 17:16:01 -0400

    Some table styling

--- src/components/TeamList.vue
+++ src/components/TeamList.vue
@@ -7,16 +7,28 @@
 <template>
 <table>
     <tr v-for="t in teams" :key="t.id">
-        <th>{{t.time}}</th>
-        <th>{{t.captain}}</th>
-        <th>{{t.pick1}}</th>
-        <th>{{t.pick2}}</th>
-        <th>{{t.pick3}}</th>
-        <th>{{t.pick4}}</th>
+        <td class="sep">{{t.time}}</td>
+        <td>{{t.captain}}</td>
+        <td>{{t.pick1}}</td>
+        <td>{{t.pick2}}</td>
+        <td>{{t.pick3}}</td>
+        <td>{{t.pick4}}</td>
     </tr>
 </table>
 </template>
 
 <style scoped>
-
+table {
+    border-spacing: 0;
+}
+td {
+    padding: 5px;
+    margin: 0;
+}
+tr:not(:last-child)>td {
+    border-bottom: solid var(--color-text);
+}
+.sep {
+   border-right: solid var(--color-text);
+}
 </style>
