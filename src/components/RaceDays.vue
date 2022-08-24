547f808a59b5baff68c518daec7fa27757626396
rsfost <81119640+rsfost@users.noreply.github.com>
2022-08-23 20:20:09 -0400

    Adding some visual depth/layers

--- src/components/RaceDays.vue
+++ src/components/RaceDays.vue
@@ -9,14 +9,14 @@ defineProps({
 </script>
 
 <template>
-    <div v-for="raceDay in raceDays">
+    <div class="raceDay" v-for="raceDay in raceDays">
         <div class="date">
             <h1>{{ prettyPrintDate(raceDay.date) }}</h1>
         </div>
         <div>
             MVP ({{ raceDay.mvp.wins }} wins): {{ raceDay.mvp.players.join(', ') }}
         </div>
-        <div v-for="(teams, key) in raceDay.races">
+        <div class="race" v-for="(teams, key) in raceDay.races">
             <h2>Race {{ key + 1 }}</h2>
             <TeamList :teams="teams"/>
         </div>
@@ -27,7 +27,19 @@ defineProps({
 .date {
     top: 0;
     position: sticky;
-    background: var(--color-background);
+    background: var(--color-background-layer1);
     z-index: 10;
+}
+
+.raceDay {
+    background: var(--color-background-layer1);
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-bottom: 20px;
+    margin-bottom: 20px;
+}
+
+.race {
+    margin-bottom: 10px;
 }
 </style>
