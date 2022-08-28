b508ecbf228592deb0e58986a371cd49f79d565f
rsfost <81119640+rsfost@users.noreply.github.com>
2022-08-28 17:12:40 -0400

    Only show top 3 in each race by default

--- src/components/TeamList.vue
+++ src/components/TeamList.vue
@@ -1,14 +1,33 @@
 <script setup>
-defineProps({
+import { computed, ref } from '@vue/reactivity';
+
+const props = defineProps({
     teams: {
         required: true
     }
 });
+
+const top3 = computed(() => {
+    return props.teams.slice(0, 3)
+});
+const losers = computed(() => {
+    return props.teams.slice(3);
+});
+
+const showLosers = ref(false);
 </script>
 
 <template>
 <table>
-    <tr v-for="t in teams" :key="t.id">
+    <tr class="top3" v-for="t in top3" :key="t.id">
+        <td class="sep">{{t.time}}</td>
+        <td>{{t.captain}}</td>
+        <td>{{t.pick1}}</td>
+        <td>{{t.pick2}}</td>
+        <td>{{t.pick3}}</td>
+        <td>{{t.pick4}}</td>
+    </tr>
+    <tr v-for="t in losers" :key="t.id" v-if="showLosers">
         <td class="sep">{{t.time}}</td>
         <td>{{t.captain}}</td>
         <td>{{t.pick1}}</td>
@@ -17,6 +36,8 @@ defineProps({
         <td>{{t.pick4}}</td>
     </tr>
 </table>
+<i class="uparrow" v-if="showLosers" @click="showLosers = false"></i>
+<i class="downarrow" v-else @click="showLosers = true"></i>
 </template>
 
 <style scoped>
@@ -32,5 +53,24 @@ tr:not(:last-child)>td {
 }
 .sep {
    border-right: solid var(--color-text);
+}
+.downarrow {
+    border: solid var(--color-text);
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+    cursor: pointer;
+}
+
+.uparrow {
+    border: solid var(--color-text);
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    cursor: pointer;
+    transform: rotate(-135deg);
+    -webkit-transform: rotate(-135deg);
 }
 </style>
