9afd19d5aa23fe1a44082b5fa73409ce1938227b
rsfost <81119640+rsfost@users.noreply.github.com>
2022-08-28 11:11:37 -0400

    Display winrate and captain leaders

--- src/components/RaceDay.vue
+++ src/components/RaceDay.vue
@@ -3,7 +3,8 @@ import TeamList from './TeamList.vue';
 import { prettyPrintDate } from '@/utils.js';
 import { ref, onMounted, watch } from 'vue';
 
-const stats = ref();
+const winrateLeaders = ref();
+const captainLeaders = ref();
 
 const props = defineProps({
     raceDay: {
@@ -13,9 +14,13 @@ const props = defineProps({
 
 onMounted(async () => {
     const localStats = await props.raceDay.stats;
-    stats.value = localStats;
-    console.log(localStats);
+    winrateLeaders.value = localStats.topWinrates;
+    captainLeaders.value = localStats.topCaptains;
 });
+
+function toPercentStr(ratio) {
+    return `${(ratio * 100).toFixed(1)}`;
+}
 </script>
 
 <template>
@@ -29,6 +34,24 @@ onMounted(async () => {
     <div class="race" v-for="(teams, key) in raceDay.races">
         <h2>Race {{ key + 1 }}</h2>
         <TeamList :teams="teams"/>
+    </div>
+    <div class="stats">
+        <div>
+            <h3 title="Past year">Top winrates</h3>
+            <ol>
+                <li v-for="leader in winrateLeaders">
+                    {{ toPercentStr(leader.winrate) }} {{ leader.player }}
+                </li>
+            </ol>
+        </div>
+        <div>
+            <h3 title="Past year">Top captains</h3>
+            <ol>
+                <li v-for="leader in captainLeaders">
+                    {{ toPercentStr(leader.pickWinrate[0]) }} {{ leader.player }}
+                </li>
+            </ol>
+        </div>
     </div>
 </div>
 
@@ -52,5 +75,10 @@ onMounted(async () => {
 
 .race {
     margin-bottom: 10px;
+}
+
+.stats {
+    display: flex;
+    flex-direction: row;
 }
 </style>
