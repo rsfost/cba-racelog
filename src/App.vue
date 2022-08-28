8dc638879634375962f6f7c3ff31be8b01c979f8
rsfost <81119640+rsfost@users.noreply.github.com>
2022-08-28 16:08:20 -0400

    Pagination!

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script setup>
 import RaceDays from './components/RaceDays.vue';
-import { ref, onMounted } from 'vue';
-import * as racelog from '@/racelog.js';
-
-const raceDays = ref();
-onMounted(async () => {
-  await racelog.init();
-  raceDays.value = await racelog.getRaceDays();
-});
 </script>
 
 <template>
   <main>
-    <RaceDays :raceDays="raceDays" />
+    <RaceDays />
   </main>
 </template>
 
--- src/components/RaceDays.vue
+++ src/components/RaceDays.vue
@@ -1,15 +1,48 @@
 <script setup>
 import RaceDay from './RaceDay.vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+import * as racelog from '@/racelog.js';
 
-defineProps({
-    raceDays: {
-        required: true
+const raceDays = ref([]);
+const scrollComponent = ref();
+const nextIndex = ref(0);
+const pageSize = 5;
+
+let nextPageLock = false;
+
+const nextPage = async () => {
+    if (nextPageLock) {
+        return;
     }
+    nextPageLock = true;
+    const nextDays = await racelog.getRaceDays(nextIndex.value, pageSize);
+    raceDays.value.push(...nextDays);
+    nextIndex.value += pageSize;
+    nextPageLock = false;
+};
+
+const handleScroll = async (e) => {
+    let element = scrollComponent.value;
+    if (element.getBoundingClientRect().bottom < window.innerHeight) {
+        await nextPage();
+    }
+};
+
+onMounted(async () => {
+    window.addEventListener("scroll", handleScroll)
+    await racelog.init();
+    await nextPage();
 });
+
+onUnmounted(() => {
+    window.removeEventListener("scroll", handleScroll)
+})
 </script>
 
 <template>
+<div ref="scrollComponent">
     <RaceDay v-for="raceDay in raceDays" :raceDay="raceDay"/>
+</div>
 </template>
 
 <style scoped>
