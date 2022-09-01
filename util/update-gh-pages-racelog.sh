edcb1e43ee25c0d6f1d55065f711c2f2da1a45d7
rsfost <81119640+rsfost@users.noreply.github.com>
2022-09-01 19:46:04 -0400

    Starting point on script for eventual racelog update automation

---
+++ util/update-gh-pages-racelog.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ "$(git branch --show-current)" != "gh-pages" ]]; then
+  echo "Error: must be on ""gh-pages"" branch"
+  exit 1
+fi
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/../docs/data
+../../util/download.py > racelog.json
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
+sha512sum racelog.json > racelog.json.sha512
+git fetch && git reset --hard origin/gh-pages
+git add -f racelog.json racelog.json.sha512
+git commit -m 'Update racelog.json'
+git push origin gh-pages
