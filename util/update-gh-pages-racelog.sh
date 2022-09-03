d7a6c70c651b2cb905442fbd53ee7536d44df66a
rsfost <81119640+rsfost@users.noreply.github.com>
2022-09-03 08:27:23 -0400

    fixing racelog update script

--- util/update-gh-pages-racelog.sh
+++ util/update-gh-pages-racelog.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+# Some failsafes to avoid accidental work loss
 if [[ "$(git branch --show-current)" != "gh-pages" ]]; then
   echo "Error: must be on ""gh-pages"" branch"
   exit 1
 fi
-# Some failsafes to avoid accidental work loss
 if [[ -n $(git status -s) ]]; then
   echo "Error: Working directory not clean"
   exit 1
@@ -14,15 +14,22 @@ if [[ -n $(git diff origin/gh-pages..HEAD) ]]; then
   exit 1
 fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Fetch latest and hard reset since this branch is force-pushed
+git fetch && git reset --hard origin/gh-pages
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
 
+# Download latest racelog from google sheets
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../docs/data
 ../../util/download.py > racelog.json
 if [[ $? -ne 0 ]]; then
   exit 1
 fi
 sha512sum racelog.json > racelog.json.sha512
-git fetch && git reset --hard origin/gh-pages
+
+# Push if any changes to racelog
 git add -f racelog.json racelog.json.sha512
 git commit -m 'Update racelog.json'
 git push origin gh-pages || git reset --hard origin/gh-pages
